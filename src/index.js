443bd1f720583e611315fcd58a3f16a9a6e7d7a1
lishiwei <lishiwei@jd.com>
2018-12-11 23:31:05 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import getCanvasById from './module/getCanvas';
+
+window.onload = () => {
+    const [header, body, footer] = ['header', 'body', 'footer']
+        .map(id => getCanvasById('header'));
+        console.log(header, body, footer);
+}
