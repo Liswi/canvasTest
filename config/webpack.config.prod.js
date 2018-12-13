b45ab5f19315f4c848c03dd251c8231d1fb9872b
lishiwei <lishiwei@jd.com>
2018-12-13 21:16:26 +0800

    日常更新

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -3,7 +3,7 @@ const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const WebpackBar = require('webpackbar');
 console.log('111111111', __dirname);
@@ -71,15 +71,15 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: root + "/src/index.html" //new 一个这个插件的实例，并传入相关的参数
         }),
-        new webpack.HotModuleReplacementPlugin(),//热加载插件
+        //new webpack.HotModuleReplacementPlugin(),//热加载插件
         new CleanWebpackPlugin(['dist', 'build'], {
             // root: path.resolve(root), // 根目录
             verbose: false,
             exclude: ['img']//不删除img静态资源
-        })
+        }),
 
         // new webpack.optimize.OccurrenceOrderPlugin(),
-
+        new ExtractTextPlugin("style.css")
     ],
     devServer: {
         //设置基本目录结构
